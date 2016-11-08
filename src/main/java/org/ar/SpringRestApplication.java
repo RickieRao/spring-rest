ef5b66a233828a2f10ee1bd38af6acb1979e9218
hua.ai <13348867336@163.com>
2016-11-08 22:02:12 +0800

    Add thymeleaf template

--- src/main/java/org/ar/SpringRestApplication.java
+++ src/main/java/org/ar/SpringRestApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @SpringBootApplication
@@ -14,8 +15,8 @@ public class SpringRestApplication {
 	private String greeting;
 
 	@RequestMapping("/")
-	public String home(){
-		return greeting;
+	public ModelAndView home(){
+		return new ModelAndView("greeting", "message", this.greeting);
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestApplication.class, args);

a08736357fee2784fb21ab954ed0c387c1454607
RickieRao <542036933@qq.com>
2016-11-08 20:28:34 +0800

    add resource to yml mapping

--- src/main/java/org/ar/SpringRestApplication.java
+++ src/main/java/org/ar/SpringRestApplication.java
@@ -2,11 +2,26 @@ package org.ar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
+@ConfigurationProperties(prefix = "spring")
 public class SpringRestApplication {
 
+	private String greeting;
+
+	@RequestMapping("/")
+	public String home(){
+		return greeting;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestApplication.class, args);
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
 	}
 }
